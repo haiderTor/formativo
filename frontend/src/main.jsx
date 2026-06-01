fc94ca448c32b27833ccbbcacae82cc2e4a78e65
haiderTor <haider.alejandro.28@gmail.com>
2026-06-01 16:19:57 -0500

    Cambio de componente en main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
-import LoginPage from './components/login/login.jsx'
-//import PrincipalPage from './components/principal/principal.jsx'
+//import LoginPage from './components/login/login.jsx'
+import PrincipalPage from './components/principal/principal.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginPage />
+    <PrincipalPage />
   </StrictMode>,
 )

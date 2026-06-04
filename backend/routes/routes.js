acd50324576382c848df85c9fe4c510afdb6c8dc
davidcelyb <celydavidbr@gmail.com>
2026-06-04 15:44:12 -0500

    cambio menor

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -65,7 +65,7 @@ app.post('/routes/clientes', async (req, res) => {
     }
 });
 
-
+/* lo que sea*/
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en el puerto ${PORT}`);

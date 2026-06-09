16d9da21a65d506130402a45621fff7b8c8c32e5
haiderTor <haider.alejandro.28@gmail.com>
2026-06-08 19:03:50 -0500

    Ajuste de renderizado de router y navegacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 // App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Principal from "./components/principal/principal.jsx";
 import LoginPage from "./components/login/login.jsx";
-import Tablero from "./components/principal/principal.jsx";
+import Tablero from "./components/tablero/tablero.jsx";
 import Personal from "./components/login/personal/personal.jsx";
 import Clientes from "./components/clientes/clientes.jsx";
 import EquipoForm from "./components/equipo/equipo.jsx";
@@ -12,11 +13,16 @@ function App() {
         <div className="flex flex-row min-h-screen bg-gray-100">
         <BrowserRouter>
             <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/tablero" element={<Tablero />} />
-            <Route path="/personal" element={<Personal />} />
-            <Route path="/clientes" element={<Clientes />} />
-            <Route path="/equipo" element={<EquipoForm />} />
+    {/* Ruta independiente para login */}
+                <Route path="/login" element={<LoginPage />} />
+
+    {/* Layout con Nav + Outlet */}
+            <Route path="/" element={<Principal />}>
+                <Route path="tablero" element={<Tablero />} />
+                <Route path="personal" element={<Personal />} />
+                <Route path="clientes" element={<Clientes />} />
+                <Route path="equipo" element={<EquipoForm />} />
+            </Route>
             </Routes>
         </BrowserRouter>
         </div>

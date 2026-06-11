aac5f2cff25ade795f22b2403940ec6b633831a9
haiderTor <haider.alejandro.28@gmail.com>
2026-06-11 00:05:18 -0500

    Resideño de interfaz

--- frontend/src/components/principal/principal.jsx
+++ frontend/src/components/principal/principal.jsx
@@ -2,93 +2,90 @@ import { NavLink, Outlet } from "react-router-dom";
 
 export default function Navegacion() {
     return (
-        <div className="flex flex-row w-full h-screen bg-[#121316]">
+        <div className="relative h-screen w-full overflow-hidden">
+            {/* Sidebar fija */}
+            <nav className="fixed top-0 left-0 h-screen text-white w-36 p-6 z-50">
+                <p className="text-2xl font-bold mb-6 text-[#FFBF00]">Centraly</p>
 
-            <nav className="text-white w-36 p-6">
-            <ul className="flex flex-col space-y-5">
+                <ul className="flex flex-col space-y-5">
                 <li>
-                <NavLink to="/tablero" className={({ isActive }) => (isActive ? "font-bold " : "") + "flex flex-row items-center justify-center"} >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 m-2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+                    <NavLink
+                    to="tablero"
+                    className={({ isActive }) =>
+                        (isActive ? "font-bold " : "") +
+                        "flex flex-row items-center justify-center"
+                    }
+                    >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-4 m-2"
+                    >
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"
+                        />
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"
+                        />
                     </svg>
                     Tablero
-                </NavLink>
+                    </NavLink>
                 </li>
                 <li>
-                <NavLink to="/inventario" className={({ isActive }) => isActive ? "font-bold" : ""}>
-                    Inventario
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to="/personal" className={({ isActive }) => isActive ? "font-bold " : ""}>
+                    <NavLink to="personal" className={({ isActive }) => isActive ? "font-bold " : ""}>
                     Personal
-                </NavLink>
+                    </NavLink>
                 </li>
                 <li>
-                <NavLink to="/clientes" className={({ isActive }) => isActive ? "font-bold " : ""}>
+                    <NavLink to="clientes" className={({ isActive }) => isActive ? "font-bold " : ""}>
                     Clientes
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to="/historial" className={({ isActive }) => isActive ? "font-bold " : ""}>
-                    Historial
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to="/ajustes" className={({ isActive }) => isActive ? "font-bold " : ""}>
-                    Ajustes
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to="/equipo" className={({ isActive }) => isActive ? "font-bold " : ""}>
-                    equipo
-                </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/reporte" className={({ isActive }) => (isActive ? "font-bold" : "")}>
-                        Reportes
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink
-                        to="/reporteclientes" className={({ isActive }) => (isActive ? "font-bold" : "")}>
-                        Reporte Clientes
+                    <NavLink to="equipo" className={({ isActive }) => isActive ? "font-bold " : ""}>
+                    Equipo
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink
-                        to="/reporteequipos" className={({ isActive }) => (isActive ? "font-bold" : "")}>
-                        Reporte Equipos
+                    <NavLink to="reporte" className={({ isActive }) => isActive ? "font-bold " : ""}>
+                    Reportes
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink
-                        to="/reporteticket" className={({ isActive }) => (isActive ? "font-bold" : "")}>
-                        Reporte Tickets
+                    <NavLink to="reporteclientes" className={({ isActive }) => isActive ? "font-bold " : ""}>
+                    Reporte Clientes
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink
-                        to="/reportefactura" className={({ isActive }) => (isActive ? "font-bold" : "")}>
-                        Reporte Facturas
+                    <NavLink to="reporteequipos" className={({ isActive }) => isActive ? "font-bold " : ""}>
+                    Reporte Equipos
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink
-                        to="/reporteservicio" className={({ isActive }) => (isActive ? "font-bold" : "")}>
-                        Reporte Servicios
+                    <NavLink to="reportefactura" className={({ isActive }) => isActive ? "font-bold " : ""}>
+                    Reporte Facturas
                     </NavLink>
                 </li>
-            </ul>
-            
+                <li>
+                    <NavLink to="reporteservicio" className={({ isActive }) => isActive ? "font-bold " : ""}>
+                    Reporte Servicios
+                    </NavLink>
+                </li>
+                </ul>
             </nav>
 
-            <div className="flex-1 p-6">
+            {/* Contenido principal con margen para no quedar debajo de la nav */}
+            <div className="flex-1 ml-36 p-3 overflow-y-auto">
                 <Outlet />
             </div>
-
         </div>
     );
 }
+
--- frontend/src/components/tablero/tablero.jsx
+++ frontend/src/components/tablero/tablero.jsx
@@ -1,7 +1,67 @@
-export default function Tablero() {
+// Dashboard.jsx
+export default function Dashboard() {
     return (
-        <div>
-            <h1>Tablero</h1>
+        <div className="bg-[#191a1d] flex flex-col w-full h-auto rounded-lg font-[Roboto Mono]"> 
+            <div className="flex flex-row  w-auto h-auto">
+                <section className="flex flex-col w-full p-6">
+                    <h2 className="flex align-items justify-between text-white text-xl font-bold">Resumen</h2>
+                    <p className="flex items-start text-gray-500">Aqui estan tus estadisticas de hoy</p>
+                </section>
+                <section className="flex flex-row gap-4 w-100 p-6">
+                    <button className="bg-[#191a1d] text-[#ffffff] font-bold h-13 py-2 px-4 rounded-lg hover:bg-[#181a1d]">
+                        Actualizar
+                    </button>
+                    <button className="bg-[#FFBF00] text-[#ffffff] font-bold py-2 px-4 rounded-lg hover:bg-[#e0a700]">
+                        Actualizar
+                    </button>
+                    
+                </section>
+            </div>
+
+            <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {/* Bloque: My Balance */}
+                <div className="bg-[#262629] h-90 rounded-xl shadow-md p-2">
+                    <h2 className="text-[#91919] text-lg font-semibold">Acumulado del Dia</h2>
+                    <section className="bg-[#1B1B1D] h-76 rounded-lg text-gray-500">
+                        <p className="text-gray-500">Wallet Overview & Spending</p>
+                        <span className="text-2xl font-bold">$28,520.30</span>
+                    </section>
+                </div>
+
+                <div className="bg-[#262629] h-90 rounded-xl shadow-md p-2">
+                    <h2 className="text-[#91919] text-lg font-semibold">Acumulado del Dia</h2>
+                    <section className="bg-[#1B1B1D] h-76 rounded-lg text-gray-500">
+                        <p className="text-gray-500">Wallet Overview & Spending</p>
+                        <span className="text-2xl font-bold">$28,520.30</span>
+                    </section>
+                </div>
+
+                                <div className="bg-[#262629] h-90 rounded-xl shadow-md p-2">
+                    <h2 className="text-[#91919] text-lg font-semibold">Acumulado del Dia</h2>
+                    <section className="bg-[#1B1B1D] h-76 rounded-lg text-gray-500">
+                        <p className="text-gray-500">Wallet Overview & Spending</p>
+                        <span className="text-2xl font-bold">$28,520.30</span>
+                    </section>
+                </div>
+
+                {/* Bloque: My Wallet */}
+                <div className="bg-[#262629] h-90 rounded-xl shadow-md p-2">
+                    <h2 className="text-[#91919] text-lg font-semibold">Acumulado del Dia</h2>
+                    <section className="bg-[#1B1B1D] h-76 rounded-lg text-gray-500">
+                        <p className="text-gray-500">Wallet Overview & Spending</p>
+                        <span className="text-2xl font-bold">$28,520.30</span>
+                    </section>
+                </div>
+
+                {/* Bloque: Cash Flow */}
+                <div className="bg-[#262629] h-90 rounded-xl shadow-md p-2">
+                    <h2 className="text-[#91919] text-lg font-semibold">Acumulado del Dia</h2>
+                    <section className="bg-[#1B1B1D] h-76 rounded-lg text-gray-500">
+                        <p className="text-gray-500">Wallet Overview & Spending</p>
+                        <span className="text-2xl font-bold">$28,520.30</span>
+                    </section>
+                </div>
+            </div>  
         </div>
     );
 }
